0382ed2d9bda6aa86e2dc3103bb82e724f986a77
lei.zhan <zhanddkk@163.com>
2018-01-29 20:40:20 +0800

    add the feature to clear bss section

--- src/main/main.c
+++ src/main/main.c
@@ -6,9 +6,14 @@
  */
 int a = 7;
 int b = 0;
+volatile float out_float = 0.0f;
 int main(void)
 {
-	a = 5;
-	b = 10;
-	return a * 5;
+	volatile int i = 0;
+	out_float = 3.14f + a + b;
+	out_float = 1.2 * out_float;
+	out_float = out_float * out_float;
+	while (1) {
+		i++;
+	}
 }
